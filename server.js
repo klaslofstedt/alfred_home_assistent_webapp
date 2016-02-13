da79ff6fbf8f1db01899fa9e4dc216603fae01d7
klaslofstedt <klaslofstedt22@gmail.com>
2016-02-13 01:17:18 +0000

    added RGB color functionality

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+// RGB variables
+var r=0, b=0, g=0;
 // set pin mode
 gpio.setup(16, gpio.DIR_OUT, write);
 
@@ -17,18 +19,73 @@ function write(){
     });
 }
     
+function RGB_calc(value){
+    if(value <= 255){
+        r=255;
+        g=value;
+        b=0;
+    }
+    else if((value > 255) && (value <= 510)){
+        r=255-(510-value);
+        g=255;
+        b=0;
+    }
+    else if((value > 510) && (value <= 765)){
+        r=0;
+        g=255;
+        b=255-(765-value);
+    }
+    else if((value > 765) && (value <= 1020)){
+        r=0;
+        g=255-(1020-value);
+        bg=255;
+    }
+    else if((value > 1020) && (value <= 1275)){
+        r=255-(1275-value);
+        g=0;
+        b=255;
+    }
+    else if((value > 1275) && (value <= 1530)){
+        r=255;
+        g=0;
+        b=1530-value;
+    }
+    else{
+        console.log('color error');
+    }
+}
 // Use the www folder
 app.use(express.static('www'));
 // Starts Socket.io server
 io.on('connection', function(socket){
     console.log('a user connected');
+    // Led 1
     socket.on('led1_brightness', function(msg){
         //gpio.write(16, msg); // make pwm
         console.log("led1_brightness = "+ msg);
     });
+    socket.on('led1_color', function(msg){
+        console.log("led1_color= "+ msg);
+        RGB_calc(msg);
+        console.log("r = "+r+" g = "+g+" b = "+b);
+    });
     socket.on('led1_status', function(msg){
         gpio.write(16, msg);
         console.log("led1_status = "+ msg);
+    });
+    // Led 2
+    socket.on('led2_brightness', function(msg){
+        //gpio.write(16, msg); // make pwm
+        console.log("led2_brightness = "+ msg);
+    });
+    socket.on('led2_color', function(msg){
+        console.log("led2_color= "+ msg);
+        RGB_calc(msg);
+        console.log("r = "+r+" g = "+g+" b = "+b);
+    });
+    socket.on('led2_status', function(msg){
+        gpio.write(16, msg);
+        console.log("led2_status = "+ msg);
     });
 });
 // Starts HTTP server that shows the app
--- www/app.js
+++ www/app.js
@@ -1,6 +1,6 @@
 (function() {
     angular.module('app', ['onsen'])
-        .controller('TodoController', function($scope, $timeout) {
+        .controller('DeviceController', function($scope, $timeout) {
             var socket = io();
             $scope.alertEdit = function(){
                 ons.createAlertDialog('alert-dialog.html').then(function(alertDialog) {
@@ -11,23 +11,43 @@
                     alertDialog.show();
                 });
             }
-            // Initial value of ON/OFF button
+            // Led 1
             $scope.led1Status = 'Off';
-            // on press function for ON/OFF button
             $scope.led1IsStatus = function(){
-                //console.log('hej');
                 $scope.led1Status = $scope.led1Status === 'On' ? 'Off' : 'On';
                 if($scope.led1Status == 'On'){
                     socket.emit('led1_status',1);
                 }
                 else{
-                    socket.emit('led',0);
+                    socket.emit('led1_status',0);
                 }
             }
-            // Initial value of the brightness bar
+            $scope.led1Color = 765;
+            $scope.led1IsColor = function() {
+                socket.emit('led1_color',$scope.led1Color);
+            } 
             $scope.led1Brightness = 50;
             $scope.led1IsBrightness = function() {
                 socket.emit('led1_brightness',$scope.led1Brightness);
+            } 
+            // Led 2
+            $scope.led2Status = 'Off';
+            $scope.led2IsStatus = function(){
+                $scope.led2Status = $scope.led2Status === 'On' ? 'Off' : 'On';
+                if($scope.led2Status == 'On'){
+                    socket.emit('led2_status',1);
+                }
+                else{
+                    socket.emit('led2_status',0);
+                }
+            }
+            $scope.led2Color = 765;
+            $scope.led2IsColor = function() {
+                socket.emit('led2_color',$scope.led2Color);
+            } 
+            $scope.led2Brightness = 50;
+            $scope.led2IsBrightness = function() {
+                socket.emit('led2_brightness',$scope.led2Brightness);
             } 
         });
 })();
